c1f383aec481aa28407be3045844f8ce73a217a7
osmanky <osmanky@users.noreply.github.com>
2021-12-16 22:07:54 +0300

    change border size

--- config.h
+++ config.h
@@ -7,7 +7,7 @@
  */
 static char *font = "mono:pixelsize=18:antialias=true:autohint=true";
 static char *font2[] = { "JoyPixels:pixelsize=18:antialias=true:autohint=true" };
-static int borderpx = 2;
+static int borderpx = 20;
 
 /*
  * What program is execed by st depends of these precedence rules:
